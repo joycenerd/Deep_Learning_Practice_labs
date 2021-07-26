cb4b2c15b3d577f93613723ab50d663c27cc2b6a
joycenerd <joycenerd.cs09@nycu.edu.tw>
2021-07-26 07:53:41 +0000

    finalize the training process

--- lab2/model.py
+++ lab2/model.py
@@ -23,7 +23,7 @@ class EEGNet(nn.Module):
             nn.AvgPool2d(kernel_size=(1,8),stride=(1,8),padding=0),
             nn.Dropout(p=0.25)
         )
-        self.classifier=nn.Linear(in_features=736,out_features=2,bias=True)
+        self.classifier=nn.Linear(in_features=736,out_features=2,bias=True)     
     
 
     def forward(self,x):
@@ -33,4 +33,61 @@ class EEGNet(nn.Module):
         x=x.view(x.shape[0],-1)
         out=self.classifier(x)
         return out
+
+
+class DeepConvNet(nn.Module):
+    def __init__(self,activation):
+        super(DeepConvNet,self).__init__()
+        self.activation=activation
+
+        self.conv1=nn.Conv2d(1,25,kernel_size=(1,5))
+        self.first_conv_block=nn.Sequential(
+            nn.Conv2d(25,25,kernel_size=(2,1)),
+            nn.BatchNorm2d(25,eps=1e-5,momentum=0.1),
+            activation,
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=0.5)
+        )
+
+        conv_layers=[]
+        channel=25
+        for i in range(4):
+            conv_layers.append(self._make_conv_block(channel))
+            channel*=2
+        self.conv_blocks=nn.Sequential(*conv_layers) 
+
+        self.classifier=nn.Linear(7600,2)  
+
+
+    def _make_conv_block(self,channel):
+        return nn.Sequential(
+            nn.Conv2d(channel,channel*2,kernel_size=(1,5)),
+            nn.BatchNorm2d(channel*2,eps=1e-5,momentum=0.1),
+            self.activation,
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=0.5)
+        ) 
+    
+
+    def forward(self,x):
+        x=self.conv1(x)
+        x=self.first_conv_block(x)
+        x=self.conv_blocks(x)
+        x=x.view(x.shape[0],-1)
+        out=self.classifier(x)
+        return out
+
+
+def initialize_weights(m):
+    if isinstance(m,nn.Conv2d):
+        nn.init.kaiming_uniform_(m.weight.data,nonlinearity="relu")
+        if m.bias is not None:
+            nn.init.constant_(m.bias.data,0)
+    elif isinstance(m,nn.BatchNorm2d):
+        nn.init.constant_(m.weight.data,1)
+        nn.init.constant_(m.bias.data,0)
+    elif isinstance(m,nn.Linear):
+        nn.init.kaiming_uniform_(m.weight.data)
+        if m.bias is not None:
+            nn.init.constant_(m.bias.data,0)
 
--- lab2/train.py
+++ lab2/train.py
@@ -1,7 +1,8 @@
 from dataloader import read_bci_data
 from dataset import EEGDataset
-from model import EEGNet
+from model import DeepConvNet, EEGNet,initialize_weights
 
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import DataLoader
 from tqdm import tqdm
@@ -15,14 +16,16 @@ import os
 
 
 parser=argparse.ArgumentParser()
-parser.add_argument("--epochs",type=int,default=300,help="number of epochs to train")
+parser.add_argument("--epochs",type=int,default=500,help="number of epochs to train")
 parser.add_argument("--act",type=str,default="elu",help="which activation function to use in the network: [elu,relu,leaky_relu]")
 parser.add_argument("--device",type=str,default="cuda",help="which device to use")
-parser.add_argument("--lr",type=float,default=1e-2,help="inital learning rate for training")
+parser.add_argument("--lr",type=float,default=1e-3,help="inital learning rate for training")
+parser.add_argument("--model",type=str,default="DeepConvNet",help="which model: [EEGNet, DeepConvNet]")
+parser.add_argument("--model_path",type=str,default="./checkpoints/EEGNet_leaky_relu_1e-2_init_amsgrad_0.8787.pt",help="checkpoint path to load for testings")
 args=parser.parse_args()
 
-save_name="EEGNet_elu_1e-2"
-writer=SummaryWriter(f"runs/{save_name}")
+save_name="DeepConvNet_elu_1e-3_amsgrad_reg1e-4"
+writer=SummaryWriter(f"runs/DeepConvNet/{save_name}")
 
 
 def train(X_train,y_train,X_test,y_test):
@@ -30,6 +33,7 @@ def train(X_train,y_train,X_test,y_test):
     act=args.act
     device=args.device
     lr=args.lr
+    _model=args.model
 
     act_dict={"elu":nn.ELU(),"relu":nn.ReLU(),"leaky_relu":nn.LeakyReLU()}
 
@@ -44,14 +48,18 @@ def train(X_train,y_train,X_test,y_test):
 
     # initialize model
     activation=act_dict[act]
-    net=EEGNet(activation)
+    if _model=="EEGNet":
+        net=EEGNet(activation)
+    elif _model=="DeepConvNet":
+        net=DeepConvNet(activation)
+    # net.apply(initialize_weights)
     net.to(device)
 
     # initialize loss function
     loss_func=nn.CrossEntropyLoss()
 
     # initialize optimizer
-    optimizer=torch.optim.Adam(net.parameters(),lr)
+    optimizer=torch.optim.Adam(net.parameters(),lr,amsgrad=True,weight_decay=1e-4)
 
     best_train_acc=0.0
     best_acc=0.0
@@ -104,8 +112,12 @@ def train(X_train,y_train,X_test,y_test):
     
     # save the best model
     print(f"best_acc: {best_acc:.4f}\t best_train_acc: {best_train_acc:.4f}")
-    torch.save(best_model_params,f"./checkpoints/{save_name}_{best_acc:.4f}.pt")
-
+    save_obj={
+        'model_name':_model,
+        'act':act_dict[act],
+        'model_state_dict':best_model_params
+    }
+    torch.save(save_obj,f"./checkpoints/{save_name}_{best_acc:.4f}.pt")
     writer.close()
 
 
@@ -133,6 +145,51 @@ def eval(net,test_loader,test_size,loss_func):
     return eval_loss,eval_acc
 
 
+def test(X_test,y_test):
+    model_path=args.model_path
+    device=args.device
+
+    # prepare data
+    test_set=EEGDataset(X_test,y_test)
+    test_loader=DataLoader(test_set,batch_size=256,shuffle=True,num_workers=4)
+    test_size=len(test_set)
+    
+    # model
+    net=load_model(model_path)
+    net.to(device)
+    net.eval()
+
+    loss_func=nn.CrossEntropyLoss()
+
+    test_loss=0.0
+    test_acc=0.0
+
+    for idx,(inputs,targets) in enumerate(tqdm(test_loader)):
+        inputs=inputs.to(device,dtype=torch.float)
+        targets=targets.to(device,dtype=torch.long)
+
+        outputs=net(inputs)
+        test_loss+=loss_func(outputs,targets).item()
+        _,predicted=torch.max(outputs.data,1)
+        test_acc+=(predicted==targets).sum().item()
+
+    test_loss/=test_size
+    test_acc/=test_size
+    print(f"test_loss: {test_loss:.4}\ttest_acc: {test_acc:.4}")
+
+    
+def load_model(model_path):
+    checkpoint=torch.load(model_path)
+    model_name=checkpoint['model_name']
+    act=checkpoint['act']
+    if model_name=="EEGNet":
+        net=EEGNet(act)
+    elif model_name=="DeepConvNet":
+        net=DeepConvNet(act)
+    net.load_state_dict(checkpoint['model_state_dict'])
+    return net   
+
+
 if __name__=="__main__":
     if not os.path.isdir("./checkpoints"):
         os.mkdir("./checkpoints")
@@ -140,3 +197,5 @@ if __name__=="__main__":
     # read data
     X_train,y_train,X_test,y_test=read_bci_data()
     train(X_train,y_train,X_test,y_test)
+    
+    # test(X_test,y_test)
