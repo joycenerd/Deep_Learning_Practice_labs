6f41ea3233a479c1fd039ca22d78e8e891833fdb
joycenerd <joycenerd.cs09@nycu.edu.tw>
2021-07-29 21:15:46 +0800

    complete data preprocessing, start model building

--- lab3/train.py
+++ lab3/train.py
@@ -1,10 +1,18 @@
 from dataloader import RetinopathyLoader
+from resnet import BottleNeckBlock
+
+from torch.utils.data import DataLoader
 
 from pathlib import Path
 
 
 def train():
+    # data preprocessing
     train_set=RetinopathyLoader("./Data/data","train")
+    train_loader=DataLoader(train_set,batch_size=4,shuffle=True,num_workers=4)
+
+    # model
+    print(BottleNeckBlock(32,64))
 
 
 
