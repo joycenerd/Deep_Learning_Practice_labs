f3c9f06bc9c39953a0e8de0579194973222d3055
joycenerd <zchin31415@gmail.com>
2021-08-02 17:37:06 +0800

    reformat code

--- lab3/resnet.py
+++ lab3/resnet.py
@@ -5,10 +5,10 @@ from functools import partial
 
 
 class Conv2dAuto(nn.Conv2d):
-    def __init__(self,*args,**kwargs):
+    def __init__(self, *args, **kwargs):
         """padding version of nn.Conv2d"""
-        super().__init__(*args,**kwargs)
-        self.padding=(self.kernel_size[0]//2,self.kernel_size[1]//2)
+        super().__init__(*args, **kwargs)
+        self.padding = (self.kernel_size[0] // 2, self.kernel_size[1] // 2)
 
 
 def act_func(act):
@@ -20,10 +20,10 @@ def act_func(act):
         corresponding Pytorch activation function
     """
     return nn.ModuleDict([
-        ['relu',nn.ReLU(inplace=True)],
-        ['leaky_relu',nn.LeakyReLU(negative_slope=0.01,inplace=True)],
-        ['selu',nn.SELU(inplace=True)]
-    ])[act] 
+        ['relu', nn.ReLU(inplace=True)],
+        ['leaky_relu', nn.LeakyReLU(negative_slope=0.01, inplace=True)],
+        ['selu', nn.SELU(inplace=True)]
+    ])[act]
 
 
 class ResidualBlock(nn.Module):
@@ -31,7 +31,8 @@ class ResidualBlock(nn.Module):
     Takes an input with in_channels, applies some blocks of convolutional layers to reduce it 
     to out_channels and sum it up to the original input.
     """
-    def __init__(self,in_channels,out_channels,expansion=1,downsampling=1,act='relu',*args,**kwargs):
+
+    def __init__(self, in_channels, out_channels, expansion=1, downsampling=1, act='relu', *args, **kwargs):
         """
         Args:
             in_channels: (int) input channel num
@@ -39,40 +40,41 @@ class ResidualBlock(nn.Module):
             expansion: (int) increase the out_channels if needed, default=1
             downsampling: (int) stride size in nn.Conv2d
         """
-        super().__init__(*args,**kwargs)
-        self.in_channels=in_channels
-        self.out_channels=out_channels
-        self.expansion=expansion
-        self.downsampling=downsampling
-        self.act=act_func(act)
+        super().__init__(*args, **kwargs)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.expansion = expansion
+        self.downsampling = downsampling
+        self.act = act_func(act)
 
-        self.blocks=nn.Identity()
+        self.blocks = nn.Identity()
         if self.should_apply_shortcut:
-            self.shortcut=nn.Sequential(
-                nn.Conv2d(self.in_channels,self.expanded_channels,kernel_size=1,stride=self.downsampling,bias=False),
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(self.in_channels, self.expanded_channels, kernel_size=1, stride=self.downsampling,
+                          bias=False),
                 nn.BatchNorm2d(self.expanded_channels)
             )
         else:
-            self.shortcut=None
-    
-    def forward(self,x):
-        residual=x
+            self.shortcut = None
+
+    def forward(self, x):
+        residual = x
         if self.should_apply_shortcut:
-            residual=self.shortcut(x)
-        x=self.blocks(x)
-        x+=residual
+            residual = self.shortcut(x)
+        x = self.blocks(x)
+        x += residual
         return x
 
     @property
     def should_apply_shortcut(self):
-        return self.in_channels!=self.expanded_channels
-       
+        return self.in_channels != self.expanded_channels
+
     @property
     def expanded_channels(self):
-        return self.out_channels*self.expansion
+        return self.out_channels * self.expansion
 
 
-def conv_bn(in_channels,out_channels,*args,**kwargs):
+def conv_bn(in_channels, out_channels, *args, **kwargs):
     """
     Stack one conv and batchnorm layer
 
@@ -83,9 +85,9 @@ def conv_bn(in_channels,out_channels,*args,**kwargs):
     Returns:
         3x3 conv layer + batchnorm
     """
-    conv=partial(Conv2dAuto,bias=False)
+    conv = partial(Conv2dAuto, bias=False)
     return nn.Sequential(
-        conv(in_channels,out_channels,*args,**kwargs),
+        conv(in_channels, out_channels, *args, **kwargs),
         nn.BatchNorm2d(out_channels)
     )
 
@@ -94,19 +96,19 @@ class BasicBlock(ResidualBlock):
     """
     Basic ResNet block composed by two layers of 3x3conv/batchnorm/activation
     """
-    expansion=1
-    
-    def __init__(self,in_channels,out_channels,*args,**kwargs):
+    expansion = 1
+
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
         """
         Args:
             in_channels: (int) input channel num
             out_channels: (int) output channel num
         """
-        super().__init__(in_channels,out_channels,*args,**kwargs)
-        self.blocks=nn.Sequential(
-            conv_bn(self.in_channels,out_channels,kernel_size=3,bias=False,stride=self.downsampling),
+        super().__init__(in_channels, out_channels, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, out_channels, kernel_size=3, bias=False, stride=self.downsampling),
             self.act,
-            conv_bn(self.out_channels,self.expanded_channels,kernel_size=3,bias=False)
+            conv_bn(self.out_channels, self.expanded_channels, kernel_size=3, bias=False)
         )
 
 
@@ -115,21 +117,21 @@ class BottleNeckBlock(ResidualBlock):
     To increase the network depth while keeping the parameters size as low as possible
     1x1conv/3x3conv/1x1conv
     """
-    expansion=4
+    expansion = 4
 
-    def __init__(self,in_channels,out_channels,*args,**kwargs):
+    def __init__(self, in_channels, out_channels, *args, **kwargs):
         """
         Args:
             in_channels: (int) input channel num
             out_channel: (int) output channel num
         """
-        super().__init__(in_channels,out_channels,expansion=4,*args,**kwargs)
-        self.blocks=nn.Sequential(
-            conv_bn(self.in_channels,self.out_channels,kernel_size=1),
+        super().__init__(in_channels, out_channels, expansion=4, *args, **kwargs)
+        self.blocks = nn.Sequential(
+            conv_bn(self.in_channels, self.out_channels, kernel_size=1),
             self.act,
-            conv_bn(self.out_channels,self.out_channels,kernel_size=3,stride=self.downsampling),
+            conv_bn(self.out_channels, self.out_channels, kernel_size=3, stride=self.downsampling),
             self.act,
-            conv_bn(self.out_channels,self.expanded_channels,kernel_size=1)
+            conv_bn(self.out_channels, self.expanded_channels, kernel_size=1)
         )
 
 
@@ -137,7 +139,8 @@ class ResNetLayer(nn.Module):
     """
     A ResNet layer composed of `n` blocks stacked one after the other
     """
-    def __init__(self,in_channels,out_channels,block=BasicBlock,n=1,*args,**kwargs):
+
+    def __init__(self, in_channels, out_channels, block=BasicBlock, n=1, *args, **kwargs):
         """
         Args:
             in_channels: (int) input channel num
@@ -146,15 +149,15 @@ class ResNetLayer(nn.Module):
             n: (int) how many blocks
         """
         super().__init__()
-        downsampling=2 if in_channels!=out_channels else 1
-        block_list=[]
-        block_list.append(block(in_channels,out_channels,*args,**kwargs,downsampling=downsampling))
-        for _ in range(n-1):
-            block_list.append(block(out_channels*block.expansion,out_channels,downsampling=1,*args,**kwargs))
-        self.blocks=nn.Sequential(*block_list)
-    
-    def forward(self,x):
-        x=self.blocks(x)
+        downsampling = 2 if in_channels != out_channels else 1
+        block_list = []
+        block_list.append(block(in_channels, out_channels, *args, **kwargs, downsampling=downsampling))
+        for _ in range(n - 1):
+            block_list.append(block(out_channels * block.expansion, out_channels, downsampling=1, *args, **kwargs))
+        self.blocks = nn.Sequential(*block_list)
+
+    def forward(self, x):
+        x = self.blocks(x)
         return x
 
 
@@ -162,7 +165,9 @@ class Encoder(nn.Module):
     """
     ResNet encoder composed by layers with increasing features
     """
-    def __init__(self,in_channels=3,blocks_sizes=[64,128,256,512],deepths=[2,2,2,2],act='relu',block=BasicBlock,*args,**kwargs):
+
+    def __init__(self, in_channels=3, blocks_sizes=[64, 128, 256, 512], deepths=[2, 2, 2, 2], act='relu',
+                 block=BasicBlock, *args, **kwargs):
         """
         Args:
             in_channels: (int) input channel, default=3 (RGB)
@@ -172,25 +177,28 @@ class Encoder(nn.Module):
             block: (nn.Module) BasicBlock or BottleNeckBlock
         """
         super().__init__()
-        self.blocks_size=blocks_sizes
-        self.act=act_func(act)
+        self.blocks_size = blocks_sizes
+        self.act = act_func(act)
 
-        self.gate=nn.Sequential(
-            nn.Conv2d(in_channels,self.blocks_size[0],kernel_size=7,stride=2,padding=3,bias=False),
+        self.gate = nn.Sequential(
+            nn.Conv2d(in_channels, self.blocks_size[0], kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(self.blocks_size[0]),
             self.act,
-            nn.MaxPool2d(kernel_size=3,stride=2,padding=1)
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
 
-        block_list=[]
-        block_list.append(ResNetLayer(blocks_sizes[0],blocks_sizes[0],n=deepths[0],act=act,block=block,*args,**kwargs))
-        for i in range(1,len(self.blocks_size)):
-            block_list.append(ResNetLayer(blocks_sizes[i-1]*block.expansion,blocks_sizes[i],n=deepths[i],act=act,block=block,*args,**kwargs))
-        self.blocks=nn.Sequential(*block_list)
+        block_list = []
+        block_list.append(
+            ResNetLayer(blocks_sizes[0], blocks_sizes[0], n=deepths[0], act=act, block=block, *args, **kwargs))
+        for i in range(1, len(self.blocks_size)):
+            block_list.append(
+                ResNetLayer(blocks_sizes[i - 1] * block.expansion, blocks_sizes[i], n=deepths[i], act=act, block=block,
+                            *args, **kwargs))
+        self.blocks = nn.Sequential(*block_list)
 
-    def forward(self,x):
-        x=self.gate(x)
-        x=self.blocks(x)
+    def forward(self, x):
+        x = self.gate(x)
+        x = self.blocks(x)
         return x
 
 
@@ -199,20 +207,21 @@ class Decoder(nn.Module):
     The tail of ResNet (classifier). It performs a global average pooling and use a fully
     connected layer to map the output to correct the class
     """
-    def __init__(self,in_features,n_classes):
+
+    def __init__(self, in_features, n_classes):
         """
         Args:
             in_features: (int) parameters in encoder
             n_classes: (int) total class num
         """
         super().__init__()
-        self.avg_pool=nn.AdaptiveAvgPool2d((1,1))
-        self.decoder=nn.Linear(in_features,n_classes)
-    
-    def forward(self,x):
-        x=self.avg_pool(x)
-        x=x.view(x.shape[0],-1)
-        x=self.decoder(x)
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+        self.decoder = nn.Linear(in_features, n_classes)
+
+    def forward(self, x):
+        x = self.avg_pool(x)
+        x = x.view(x.shape[0], -1)
+        x = self.decoder(x)
         return x
 
 
@@ -220,23 +229,24 @@ class ResNet(nn.Module):
     """
     ResNet model class
     """
-    def __init__(self,in_channels,n_classes,*args,**kwargs):
+
+    def __init__(self, in_channels, n_classes, *args, **kwargs):
         """
         Args:
             in_channels: (int) input channel num
             n_classes: (int) total class
         """
         super().__init__()
-        self.encoder=Encoder(in_channels,*args,**kwargs)
-        self.decoder=Decoder(self.encoder.blocks[-1].blocks[-1].expanded_channels,n_classes)
+        self.encoder = Encoder(in_channels, *args, **kwargs)
+        self.decoder = Decoder(self.encoder.blocks[-1].blocks[-1].expanded_channels, n_classes)
 
-    def forward(self,x):
-        x=self.encoder(x)
-        x=self.decoder(x)
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
         return x
 
 
-def resnet18(in_channels,n_classes,block=BasicBlock,*args,**kwargs):
+def resnet18(in_channels, n_classes, block=BasicBlock, *args, **kwargs):
     """
     ResNet18 model
     
@@ -248,10 +258,10 @@ def resnet18(in_channels,n_classes,block=BasicBlock,*args,**kwargs):
     Returns:
         ReNet model with resnet18 specific layers
     """
-    return ResNet(in_channels,n_classes,block=block,deepths=[2,2,2,2],*args,**kwargs)
+    return ResNet(in_channels, n_classes, block=block, deepths=[2, 2, 2, 2], *args, **kwargs)
 
 
-def resnet50(in_channels,n_classes,block=BottleNeckBlock,*args,**kwargs):
+def resnet50(in_channels, n_classes, block=BottleNeckBlock, *args, **kwargs):
     """
     ResNet50 model
     
@@ -263,7 +273,7 @@ def resnet50(in_channels,n_classes,block=BottleNeckBlock,*args,**kwargs):
     Returns:
         ReNet model with resnet18 specific layers
     """
-    return ResNet(in_channels,n_classes,block=block,deepths=[3,4,6,3],*args,**kwargs)
+    return ResNet(in_channels, n_classes, block=block, deepths=[3, 4, 6, 3], *args, **kwargs)
 
 
 def pretrained_resnet(_model):
@@ -278,26 +288,26 @@ def pretrained_resnet(_model):
         return pretrained model + modification of last fc layer with initialization
 
     """
-    if _model=="resnet18":
-        model=models.resnet18(pretrained=True)
-    elif _model=="resnet50":
-        model=models.resnet50(pretrained=True)
+    if _model == "resnet18":
+        model = models.resnet18(pretrained=True)
+    elif _model == "resnet50":
+        model = models.resnet50(pretrained=True)
 
-    num_feat=model.fc.in_features
-    model.fc=nn.Linear(num_feat,5)
+    num_feat = model.fc.in_features
+    model.fc = nn.Linear(num_feat, 5)
     nn.init.xavier_uniform_(model.fc.weight)
     model.fc.bias.data.fill_(0.001)
 
     return model
 
 
-def funct(list_mods,act):
+def funct(list_mods, act):
     print("type: ", type(list_mods))
     for i in range(len(list_mods)):
         if list_mods[i].__class__.__name__ == "ReLU":
             list_mods[i] = act_func(act)
-        elif list_mods[i].__class__.__name__ in ("Sequential", "BasicBlock","Bottleneck"):
-            list_mods[i] = nn.Sequential(*funct(list(list_mods[i].children()),act))
+        elif list_mods[i].__class__.__name__ in ("Sequential", "BasicBlock", "Bottleneck"):
+            list_mods[i] = nn.Sequential(*funct(list(list_mods[i].children()), act))
     return list_mods
 
 
@@ -319,7 +329,3 @@ def initialize_weights(m):
         nn.init.xavier_uniform_(m.weight.data)
         if m.bias is not None:
             nn.init.constant_(m.bias.data, 0)
-
-
-
-
--- lab3/train.py
+++ lab3/train.py
@@ -1,4 +1,4 @@
-from resnet import resnet18,resnet50,pretrained_resnet,initialize_weights,funct
+from resnet import resnet18, resnet50, pretrained_resnet, initialize_weights, funct
 from dataloader import RetinopathyLoader
 
 from torch.utils.tensorboard import SummaryWriter
@@ -14,113 +14,112 @@ import logging
 import copy
 import os
 
-
-parser=argparse.ArgumentParser()
-parser.add_argument("--model",type=str,default="resnet18",help="which model: [resnet18, resnet50]")
-parser.add_argument("--pretrain",type=bool,default=False,help="whether to use pretrained weights")
-parser.add_argument("--act",type=str,default='relu',help="which activation function to use: [relu, leaky_relu,selu]")
-parser.add_argument("--device",type=str,default="cuda:1",help="use which device for training")
-parser.add_argument("--batch-size",type=int,default=32,help="batch size for training")
-parser.add_argument("--lr",type=float,default=1e-3,help="learning rata")
-parser.add_argument("--epochs",type=int,default=10,help="num of epochs for training")
-parser.add_argument("--model-path",type=str,default="./checkpoints/resnet50/relu_0.001_0.7335.pt")
-parser.add_argument("--load",type=bool,default=True,help="if load the weight param from checkpoint before training")
-parser.add_argument("--img-size",type=int,default=256,help="Size to resize the image")
-args=parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--model", type=str, default="resnet18", help="which model: [resnet18, resnet50]")
+parser.add_argument("--pretrain", type=bool, default=False, help="whether to use pretrained weights")
+parser.add_argument("--act", type=str, default='relu', help="which activation function to use: [relu, leaky_relu,selu]")
+parser.add_argument("--device", type=str, default="cuda:1", help="use which device for training")
+parser.add_argument("--batch-size", type=int, default=32, help="batch size for training")
+parser.add_argument("--lr", type=float, default=1e-3, help="learning rata")
+parser.add_argument("--epochs", type=int, default=10, help="num of epochs for training")
+parser.add_argument("--model-path", type=str, default="./checkpoints/resnet50/relu_0.001_0.7335.pt")
+parser.add_argument("--load", type=bool, default=True, help="if load the weight param from checkpoint before training")
+parser.add_argument("--img-size", type=int, default=256, help="Size to resize the image")
+args = parser.parse_args()
 
 
 def train():
-    _model=args.model
-    pretrain=args.pretrain
-    act=args.act
-    device=args.device
-    batch_size=args.batch_size
-    lr=args.lr
-    epochs=args.epochs
-    load=args.load
-    model_path=args.model_path
-    img_size=args.img_size
+    _model = args.model
+    pretrain = args.pretrain
+    act = args.act
+    device = args.device
+    batch_size = args.batch_size
+    lr = args.lr
+    epochs = args.epochs
+    load = args.load
+    model_path = args.model_path
+    img_size = args.img_size
 
     if pretrain:
-        save_name=f"{act}_{lr}"
+        save_name = f"{act}_{lr}"
     else:
-        save_name=f"{act}_{lr}"
-    writer=SummaryWriter(f"runs/{_model}/{save_name}")
-    logging.basicConfig(format='%(asctime)s - %(message)s', 
+        save_name = f"{act}_{lr}"
+    writer = SummaryWriter(f"runs/{_model}/{save_name}")
+    logging.basicConfig(format='%(asctime)s - %(message)s',
                         level=logging.INFO,
-                        handlers=[logging.FileHandler(f'./record/{save_name}.log','w','utf-8')])
+                        handlers=[logging.FileHandler(f'./record/{save_name}.log', 'w', 'utf-8')])
 
     # data preprocessing
-    train_set=RetinopathyLoader("./Data/data","train",img_size)
-    train_loader=DataLoader(train_set,batch_size=batch_size,shuffle=True,num_workers=4)
-    test_set=RetinopathyLoader("./Data/data","test",img_size)
-    test_loader=DataLoader(test_set,batch_size=batch_size,shuffle=False,num_workers=4)
+    train_set = RetinopathyLoader("./Data/data", "train", img_size)
+    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_set = RetinopathyLoader("./Data/data", "test", img_size)
+    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=4)
 
-    train_size=len(train_set)
-    test_size=len(test_set)
+    train_size = len(train_set)
+    test_size = len(test_set)
 
     # initialize model
     if load:
-        model=load_model(model_path)
+        model = load_model(model_path)
     elif pretrain:
-        model=pretrained_resnet(_model)
+        model = pretrained_resnet(_model)
     else:
-        if _model=="resnet18":
-            model=resnet18(3,5,act=act)
-        elif _model=="resnet50":
-            model=resnet50(3,5,act=act)
+        if _model == "resnet18":
+            model = resnet18(3, 5, act=act)
+        elif _model == "resnet50":
+            model = resnet50(3, 5, act=act)
         model.apply(initialize_weights)
-    model=model.to(device)
+    model = model.to(device)
 
     # initialize loss function
-    loss_func=nn.CrossEntropyLoss()
+    loss_func = nn.CrossEntropyLoss()
 
     # initialize optimizer
-    optimizer=torch.optim.SGD(model.parameters(),lr,momentum=0.9,weight_decay=5e-4)
+    optimizer = torch.optim.SGD(model.parameters(), lr, momentum=0.9, weight_decay=5e-4)
 
     # initialize scheduler
-    scheduler=StepLR(optimizer,step_size=2,gamma=0.5)
+    scheduler = StepLR(optimizer, step_size=2, gamma=0.5)
 
-    best_train_acc=0.0
-    best_acc=0.0
-    best_model_params=copy.deepcopy(model.state_dict())
+    best_train_acc = 0.0
+    best_acc = 0.0
+    best_model_params = copy.deepcopy(model.state_dict())
 
-    for epoch in range(1,epochs+1):
+    for epoch in range(1, epochs + 1):
         print(f"Epoch {epoch}/{epochs}")
-        print("-"*len(f"Epoch {epoch}/{epochs}"))
+        print("-" * len(f"Epoch {epoch}/{epochs}"))
         logging.info(f"Epoch {epoch}/{epochs}")
 
-        train_loss=0.0
-        train_acc=0.0
+        train_loss = 0.0
+        train_acc = 0.0
 
         # training
         model.train()
-        for idx,(inputs,targets) in enumerate(tqdm(train_loader)):
-            inputs=inputs.to(device,dtype=torch.float)
-            targets=targets.to(device,dtype=torch.long)
+        for idx, (inputs, targets) in enumerate(tqdm(train_loader)):
+            inputs = inputs.to(device, dtype=torch.float)
+            targets = targets.to(device, dtype=torch.long)
 
             # forward pass
-            outputs=model(inputs)
-            loss=loss_func(outputs,targets)
-            train_loss+=loss.item()*inputs.shape[0]
-            _,predicted=torch.max(outputs.data,1)
-            train_acc+=(predicted==targets).sum().item()
+            outputs = model(inputs)
+            loss = loss_func(outputs, targets)
+            train_loss += loss.item() * inputs.shape[0]
+            _, predicted = torch.max(outputs.data, 1)
+            train_acc += (predicted == targets).sum().item()
 
             # update the parameters
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-        train_loss/=train_size
-        train_acc/=train_size
+        train_loss /= train_size
+        train_acc /= train_size
         print(f"train_loss: {train_loss:.4f}\ttrain_acc: {train_acc:.4f}")
         logging.info(f"train_loss: {train_loss:.4f}\ttrain_acc: {train_acc:.4f}")
-        
+
         # evaluation
-        eval_loss,eval_acc=eval(model,test_loader,test_size,loss_func)
+        eval_loss, eval_acc = eval(model, test_loader, test_size, loss_func)
         logging.info(f"eval_loss: {eval_loss:.4f}\teval_acc: {eval_acc:.4f}")
 
-        if epoch>=4:
+        if epoch >= 4:
             scheduler.step()
 
         # log the result
@@ -132,18 +131,17 @@ def train():
         writer.flush()
 
         # save model parameters if accuracy is higher
-        if eval_acc>best_acc:
-            best_model_params=copy.deepcopy(model.state_dict())
-            best_acc=eval_acc
-            best_train_acc=train_acc
-        
+        if eval_acc > best_acc:
+            best_model_params = copy.deepcopy(model.state_dict())
+            best_acc = eval_acc
+            best_train_acc = train_acc
+
     # save the best model
     print(f"best_acc: {best_acc:.4f}\tbest_train_acc: {best_train_acc:.4f}")
     logging.info(f"best_acc: {best_acc:.4f}\tbest_train_acc: {best_train_acc:.4f}")
-    save_model(_model,best_model_params,pretrain,act,save_name,best_acc)
+    save_model(_model, best_model_params, pretrain, act, save_name, best_acc)
 
     writer.close()
-
 
 
 def eval(model, test_loader, test_size, loss_func):
@@ -170,7 +168,7 @@ def eval(model, test_loader, test_size, loss_func):
         targets = targets.to(device, dtype=torch.long)
 
         outputs = model(inputs)
-        eval_loss += loss_func(outputs, targets).item()*inputs.shape[0]
+        eval_loss += loss_func(outputs, targets).item() * inputs.shape[0]
         _, predicted = torch.max(outputs.data, 1)
         eval_acc += (predicted == targets).sum().item()
 
@@ -185,41 +183,41 @@ def test():
     """
     Test the saving model
     """
-    model_path=args.model_path
-    device=args.device
-    batch_size=args.batch_size
-    img_size=args.img_size
+    model_path = args.model_path
+    device = args.device
+    batch_size = args.batch_size
+    img_size = args.img_size
 
     # prepare data
-    test_set=RetinopathyLoader("./Data/data","test",img_size)
-    test_loader=DataLoader(test_set,batch_size=batch_size,num_workers=4)
-    test_size=len(test_set)
+    test_set = RetinopathyLoader("./Data/data", "test", img_size)
+    test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=4)
+    test_size = len(test_set)
 
     # load the model
-    model=load_model(model_path)
+    model = load_model(model_path)
     model.to(device)
     model.eval()
 
-    loss_func=nn.CrossEntropyLoss()
+    loss_func = nn.CrossEntropyLoss()
 
-    test_loss=0.0
-    test_acc=0.0
+    test_loss = 0.0
+    test_acc = 0.0
 
-    for idx,(inputs,targets) in enumerate(tqdm(test_loader)):
+    for idx, (inputs, targets) in enumerate(tqdm(test_loader)):
         inputs = inputs.to(device, dtype=torch.float)
         targets = targets.to(device, dtype=torch.long)
 
         outputs = model(inputs)
-        test_loss += loss_func(outputs, targets).item()*inputs.shape[0]
+        test_loss += loss_func(outputs, targets).item() * inputs.shape[0]
         _, predicted = torch.max(outputs.data, 1)
         test_acc += (predicted == targets).sum().item()
 
     test_loss /= test_size
     test_acc /= test_size
     print(f"test_loss: {test_loss:.4}\ttest_acc: {test_acc:.4}")
- 
 
-def save_model(_model,model_params,pretrain,act,save_name,best_acc):
+
+def save_model(_model, model_params, pretrain, act, save_name, best_acc):
     """
     Save the model as checkpoint
 
@@ -231,14 +229,14 @@ def save_model(_model,model_params,pretrain,act,save_name,best_acc):
         save_name: (str) saving name of this checkpoint file
         best_acc: (float) best evaluation accuracy
     """
-    save_obj={
-        'model_state_dict':model_params,
-        'model_name':_model,
-        'pretrained':pretrain,
-        'act':act
+    save_obj = {
+        'model_state_dict': model_params,
+        'model_name': _model,
+        'pretrained': pretrain,
+        'act': act
     }
 
-    torch.save(save_obj,f"./checkpoints/{_model}/{save_name}_{best_acc:.4f}.pt")
+    torch.save(save_obj, f"./checkpoints/{_model}/{save_name}_{best_acc:.4f}.pt")
 
 
 def load_model(model_path):
@@ -251,25 +249,23 @@ def load_model(model_path):
     Returns:
         model with saving params
     """
-    checkpoint=torch.load(model_path)
-    _model=checkpoint['model_name']
-    pretrain=checkpoint['pretrained']
-    act=checkpoint['act']
-    
+    checkpoint = torch.load(model_path)
+    _model = checkpoint['model_name']
+    pretrain = checkpoint['pretrained']
+    act = checkpoint['act']
+
     if pretrain:
-        model=pretrained_resnet(_model)
+        model = pretrained_resnet(_model)
     else:
-        if _model=="resnet18":
-            model=resnet18(3,5,act=act)
-        elif _model=="resnet50":
-            model=resnet50(3,5,act=act)
+        if _model == "resnet18":
+            model = resnet18(3, 5, act=act)
+        elif _model == "resnet50":
+            model = resnet50(3, 5, act=act)
     model.load_state_dict(checkpoint['model_state_dict'])
     return model
 
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     print(args)
     input("Press ENTER if no problem...")
 
